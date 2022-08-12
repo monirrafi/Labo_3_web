1aeada164cdff2403928e459f1fc82c11c5bd516
monirrafi <programermonirrafi@gmail.com>
2022-08-11 23:06:26 -0400

    Jeudi

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+let basculerVisibilteModal = (idModal, etatFR) => {
+    let idModalJQ = "#"+idModal;
+    let etatEN = (etatFR == 'montrer')?'show':'hide';
+    $(idModalJQ).modal(etatEN);  
+  }
+  let retour = () => {
+    window.open("../index.html");
+ }
+ 
